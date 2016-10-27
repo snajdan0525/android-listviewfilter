000d2ae20c918b768a94cb41a4796746873c66b4
snajdan890525 <435063994@qq.com>
2016-10-27 21:06:37 +0800

    add PinnedHeaderListView

---
+++ src/com/snalopainen/android_listview_filter/PinnedHeaderListView.java
@@ -0,0 +1,32 @@
+package com.snalopainen.android_listview_filter;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+/*
+ * @author: snalopainen
+ * @email: snajdan0525@gmail.com
+ * @github: https://github.com/snalopainen
+ * 
+ * 
+ * a ListView hold a header pinned view at the top of the ListView.
+ * 
+ */
+public class PinnedHeaderListView extends ListView {
+	
+	public PinnedHeaderListView(Context context) {
+		super(context);
+	}
+
+	public PinnedHeaderListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PinnedHeaderListView(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
